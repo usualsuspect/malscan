e2e5c4602264f93b3c2d0ec6563c8e6878502faa
Johann Aydinbas <jsa@aydinbas.de>
2018-05-13 00:22:46 +0200

    Initial

---
+++ malscan/malscan.cpp
@@ -0,0 +1,12 @@
+﻿// malscan.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "malscan.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ malscan/malscan.h
@@ -0,0 +1,8 @@
+﻿// malscan.h: Includedatei für Standardsystem-Includedateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die das Programm benötigt.
